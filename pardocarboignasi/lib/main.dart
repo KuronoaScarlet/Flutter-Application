ece10cf643c14554c223f6fa717dc0848c4aa696
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2021-10-20 11:29:33 +0200

    Finished v0.8
    - Menos roundness en las cajas en general.
    - BoolText de las midboxes mal alineado.
    - Comenzar a perfeccionarlo

--- pardocarboignasi/lib/main.dart
+++ pardocarboignasi/lib/main.dart
@@ -20,105 +20,7 @@ class MyApp extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             children: [
               TopContainer(),
-              Expanded(
-                flex: 10,
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Text(
-                        "Rooms",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 12,
-                            ),
-                            width: 120,
-                            decoration: BoxDecoration(
-                              color: Colors.black12,
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(25),
-                              ),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisSize: MainAxisSize.max,
-                                  children: const [
-                                    Icon(
-                                      Icons.living_outlined,
-                                      size: 30,
-                                    ),
-                                    SizedBox(
-                                      width: 20,
-                                    ),
-                                    Text(
-                                      "On",
-                                      style: TextStyle(fontSize: 14),
-                                    ),
-                                    Icon(Icons.check_box),
-                                  ],
-                                ),
-                                Text(""),
-                                Text(""),
-                                Row(),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                      Row(),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: Colors.black87,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(20),
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          mainAxisSize: MainAxisSize.max,
-                          children: const [
-                            MenuIcons(
-                              concept: "Home",
-                              icon: Icons.home_filled,
-                              color: Colors.white,
-                            ),
-                            MenuIcons(
-                              concept: "New device",
-                              icon: Icons.add_box_outlined,
-                            ),
-                            MenuIcons(
-                              concept: "Settings",
-                              icon: Icons.settings,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              BottomContainer(),
             ],
           ),
         ),
@@ -127,41 +29,304 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MenuIcons extends StatelessWidget {
-  final String concept;
-  final IconData icon;
-  final Color? color;
-  const MenuIcons({
+class BottomContainer extends StatelessWidget {
+  const BottomContainer({
     Key? key,
-    required this.concept,
-    required this.icon,
-    this.color = Colors.white70,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          size: 30,
-          color: color,
+    return Expanded(
+      flex: 10,
+      child: Container(
+        padding: EdgeInsets.fromLTRB(8, 15, 5, 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              "Rooms",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                InfoMidBox(
+                  roomIcon: Icons.living_outlined,
+                  boolText: "On",
+                  checkBox: Icons.check_box,
+                  roomName: "Living Room",
+                  devicesValue: "5 devices",
+                  tempValue: "24 ºC",
+                  humValue: "18%",
+                  enerValue: "56 kWh",
+                  boxColor: Colors.blue[500],
+                  topTextColor: Colors.white,
+                  topIconColor: Colors.white,
+                  botTextColor: Colors.white,
+                  botIconColor: Colors.white,
+                ),
+                InfoMidBox(
+                  roomIcon: Icons.bed,
+                  boolText: "Off",
+                  checkBox: Icons.check_box_outline_blank_rounded,
+                  roomName: "Bedroom",
+                  devicesValue: "3 devices",
+                  tempValue: "19 ºC",
+                  humValue: "31%",
+                  enerValue: "21 kWh",
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                InfoMidBox(
+                  roomIcon: Icons.bathtub_outlined,
+                  boolText: "Off",
+                  checkBox: Icons.check_box_outline_blank_rounded,
+                  roomName: "Bathroom",
+                  devicesValue: "3 devices",
+                  tempValue: "11 ºC",
+                  humValue: "67%",
+                  enerValue: "19 kWh",
+                ),
+                InfoMidBox(
+                  roomIcon: Icons.kitchen_outlined,
+                  boolText: "Off",
+                  checkBox: Icons.check_box_outline_blank_rounded,
+                  roomName: "Kitchen",
+                  devicesValue: "6 devices",
+                  tempValue: "26 ºC",
+                  humValue: "48%",
+                  enerValue: "98 kWh",
+                )
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.black87,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.max,
+                children: const [
+                  Draw(
+                    value: "Home",
+                    icon: Icons.home_filled,
+                    color: Colors.white,
+                  ),
+                  Draw(
+                    value: "New device",
+                    icon: Icons.add_box_outlined,
+                    color: Colors.white70,
+                  ),
+                  Draw(
+                    value: "Settings",
+                    icon: Icons.settings,
+                    color: Colors.white70,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        SizedBox(
-          height: 4,
+      ),
+    );
+  }
+}
+
+class InfoMidBox extends StatelessWidget {
+  final IconData roomIcon;
+  final String boolText;
+  final IconData checkBox;
+  final String roomName;
+  final String devicesValue;
+  final String tempValue;
+  final String humValue;
+  final String enerValue;
+  final Color? boxColor;
+  final Color? topIconColor;
+  final Color? botIconColor;
+  final Color? topTextColor;
+  final Color? botTextColor;
+
+  const InfoMidBox({
+    required this.roomIcon,
+    required this.boolText,
+    required this.checkBox,
+    required this.roomName,
+    required this.devicesValue,
+    required this.tempValue,
+    required this.humValue,
+    required this.enerValue,
+    this.boxColor = Colors.black12,
+    this.topIconColor = Colors.black87,
+    this.botIconColor = Colors.black45,
+    this.topTextColor = Colors.black54,
+    this.botTextColor = Colors.black45,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 15,
         ),
-        Text(
-          concept,
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-            color: color,
+        margin: EdgeInsets.symmetric(horizontal: 5),
+        width: 120,
+        decoration: BoxDecoration(
+          color: boxColor,
+          borderRadius: BorderRadius.all(
+            Radius.circular(25),
           ),
         ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Icon(
+                  roomIcon,
+                  size: 30,
+                  color: topIconColor,
+                ),
+                Text(
+                  boolText,
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: topTextColor,
+                  ),
+                ),
+                Icon(
+                  checkBox,
+                  color: topIconColor,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              roomName,
+              style: TextStyle(
+                color: topTextColor,
+                fontSize: 28,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              devicesValue,
+              style: TextStyle(
+                color: botTextColor,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            InfoIcons(
+              temperature: tempValue,
+              humidity: humValue,
+              energy: enerValue,
+              color: botIconColor,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InfoIcons extends StatelessWidget {
+  final String temperature;
+  final String humidity;
+  final String energy;
+  final Color? color;
+  const InfoIcons({
+    required this.temperature,
+    required this.humidity,
+    required this.energy,
+    this.color = Colors.black45,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Draw(
+          icon: Icons.device_thermostat_rounded,
+          value: temperature,
+          color: color,
+        ),
+        Draw(
+          icon: Icons.water_damage_sharp,
+          value: humidity,
+          color: color,
+        ),
+        Draw(
+          icon: Icons.electrical_services_rounded,
+          value: energy,
+          color: color,
+        ),
       ],
+    );
+  }
+}
+
+class Draw extends StatelessWidget {
+  final IconData icon;
+  final String value;
+  final Color? color;
+
+  const Draw({
+    Key? key,
+    required this.icon,
+    required this.value,
+    this.color = Colors.black45,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            color: color,
+          ),
+          Text(
+            value,
+            style: TextStyle(
+              color: color,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -175,12 +340,12 @@ class TopContainer extends StatelessWidget {
     double hPadding = 10;
     double vPadding = 30;
     return Expanded(
-      flex: 8,
+      flex: 7,
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: hPadding, vertical: vPadding),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           mainAxisSize: MainAxisSize.min,
           children: [
             Row(
@@ -190,6 +355,9 @@ class TopContainer extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: const [
+                    SizedBox(
+                      height: 40,
+                    ),
                     Text(
                       "Good Evening",
                       style: TextStyle(
@@ -208,6 +376,7 @@ class TopContainer extends StatelessWidget {
                   ],
                 ),
                 Container(
+                  margin: EdgeInsets.only(top: 40),
                   width: 90,
                   height: 90,
                   decoration: BoxDecoration(
@@ -216,6 +385,9 @@ class TopContainer extends StatelessWidget {
                   ),
                 )
               ],
+            ),
+            SizedBox(
+              height: 40,
             ),
             Text(
               "Overview",
@@ -272,41 +444,40 @@ class InfoTopBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Row(
-            children: [
-              Icon(icon, color: Colors.blue[500], size: 30),
-            ],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Text(
-            concept,
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 14,
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.all(8),
+        margin: EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(icon, color: Colors.blue[500], size: 30),
+            SizedBox(
+              height: 20,
             ),
-          ),
-          Text(
-            number,
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 22,
-              fontWeight: FontWeight.w600,
+            Text(
+              concept,
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 14,
+              ),
             ),
+            Text(
+              number,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(15),
           ),
-        ],
-      ),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(
-          Radius.circular(15),
         ),
       ),
     );
